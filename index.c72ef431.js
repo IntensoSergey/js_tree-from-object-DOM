ebbc40f745beda67c722ebfea7e4d5fca169e741
Serhii Prikhodko <venom99@ukr.net>
2024-06-19 14:27:06 +0300

    make build

---
+++ index.c72ef431.js
@@ -0,0 +1,2 @@
+!function e(n,r){for(var t in r){var a=document.createElement("ul"),c=document.createElement("li");c.innerText=t,a.append(c),n.append(a),Object.keys(r[t]).length>0&&e(c,r[t])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.c72ef431.js.map
